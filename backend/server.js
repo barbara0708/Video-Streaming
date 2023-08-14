59fe9569d90c3933946162684ba4e04ffa7d57e5
barbara <marcelineabadir527@gmail.com>
2023-08-14 12:57:43 +0200

    login & signup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express=require('express')
 const dotenv=require('dotenv').config()
 const sequelize=require('sequelize')
 const cookieParser=require('cookie-parser')
+const db=require('./models')
+const userRoutes=require('./routes/userRoutes')
 
 const PORT=process.env.PORT||8080
 const app=express()
@@ -9,5 +11,11 @@ const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+
+db.sequelize.sync({force:true}).then(()=>{
+    console.log('db has been re sync')
+});
+
+app.use('api/users',userRoutes)
 
 app.listen(PORT,()=>{`Parser is available on ${PORT}`})
