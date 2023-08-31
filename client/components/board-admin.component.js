c90d60a62298c9200e75a7f9af7d0d593398e22b
barbara <marcelineabadir527@gmail.com>
2023-08-31 12:56:51 +0200

    components

---
+++ client/components/board-admin.component.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import UserService from "../services/user.service"
+
+export default class BoardAdmin extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            content:""
+        }
+    }
+    componentDidMount(){
+        UserService.getAdminBoard().then(
+            (response)=>{
+                this.setState({
+                    content:response.data
+                })
+            },
+            error=>{
+                this.setState({
+                    content:
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString()
+                })
+            }
+        )
+    }
+    render(){
+        return(
+            <div class="container">
+                <header class="jumbotron">
+               <h3>{this.state.content}</h3>
+               </header> 
+            </div>
+            
+        )
+    }
+}
