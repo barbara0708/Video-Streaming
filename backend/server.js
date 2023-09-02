571698789bc97f81493b1bbffaab4dfcef199828
barbara <marcelineabadir527@gmail.com>
2023-09-02 13:03:02 +0200

    authentication

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors=require('cors')
 const dotenv=require('dotenv').config()
 const db=require('./models')
 const router=require('./routes/userRoutes')
+const { defaults } = require('pg')
 const PORT=process.env.PORT||8080
 const Role=db.role
 
@@ -14,24 +15,37 @@ app.use(express.json())
 app.use('/',router)
 app.use(express.static(path.resolve(__dirname,"../client/build")))
 
-db.sequelize.sync({force:true}).then(()=>{
+db.sequelize.sync().then(()=>{
     console.log('db has been re sync')
     init();
 });
 
 function init(){
-    Role.create({
+    Role.findOrCreate({
+        where:{
+            name:"user"
+        },
+        defaults:{
         id:1,
         name:'user'
-    });
-    Role.create({
+    }});
+    Role.findOrCreate(
+        {
+            where:{
+                name:"moderator"
+            },
+            defaults:{
         id:2,
         name:"moderator"
-    });
-    Role.create({
+    }});
+    Role.findOrCreate({
+        where:{
+            name:"admin"
+        },
+        defaults:{
         id:3,
         name:"admin"
-    });
+    }});
 }
 
 app.listen(PORT,()=>{console.log(`Parser is available on ${PORT}`)})
