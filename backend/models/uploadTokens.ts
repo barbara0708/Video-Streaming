05e6cda133aa197bbac09e793d51320bab51bae4
barbara <marcelineabadir527@gmail.com>
2023-09-04 12:34:46 +0200

    api.video

--- backend/models/uploadTokens.ts
+++ backend/models/uploadTokens.ts
@@ -1,5 +1,17 @@
 import ApiVideoClient from "@api.video/nodejs-client";
+import {Request, Response} from "express";
 
 export default async function handle(req:Request,res:Response) {
-    
+    try{
+        const client=new ApiVideoClient({
+            apiKey:process.env.API_KEY
+        })
+        if(req.method==="GET"){
+            const uploadTokenList= await client.uploadTokens.list();
+            res.status(200).json({})
+        }
+    }catch(error){
+        res.status(400).send(console.error);
+
+    }
 }
